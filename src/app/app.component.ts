e4587fc8e61b61a701339dd7e010152c1aed5c7c
Abhijeet-Avee <yashsingh461996@gmail.com>
2025-02-09 21:58:36 +0530

    banner and marquee added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { HeaderComponent } from "./features/header/header/header.component";
+import { MarqueeComponent } from "./features/marquee/marquee.component";
+import { BannerComponent } from "./core/banner/banner.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatCardModule, HeaderComponent, HeaderComponent],
+  imports: [RouterOutlet, MatCardModule, HeaderComponent, HeaderComponent, MarqueeComponent, BannerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
